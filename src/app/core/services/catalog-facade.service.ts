452fb5f7f4aae08c8784d5348a499aa985cd6be4
tuan-haui <2022605037.dhk17@st.haui.edu.vn>
2026-05-05 00:51:15 +0700

    update

--- src/app/core/services/catalog-facade.service.ts
+++ src/app/core/services/catalog-facade.service.ts
@@ -35,7 +35,7 @@ export class CatalogFacadeService {
   }
 
   getCustomers(companyId: string, keyword = ''): Observable<CustomerDto[]> {
-    return this.client.customersGET(companyId, keyword || undefined).pipe(map((r) => r.data ?? []));
+    return this.client.getCustomerByCompany(companyId, keyword || undefined).pipe(map((r) => r.data ?? []));
   }
 
   createCustomer(payload: CreateCustomerCommand): Observable<void> {
@@ -51,7 +51,7 @@ export class CatalogFacadeService {
   }
 
   getProducts(companyId: string, keyword = ''): Observable<ProductDto[]> {
-    return this.client.productsGET(companyId, keyword || undefined).pipe(map((r) => r.data ?? []));
+    return this.client.products(companyId, keyword || undefined).pipe(map((r) => r.data ?? []));
   }
 
   createProduct(payload: CreateProductCommand): Observable<ProductDto> {
