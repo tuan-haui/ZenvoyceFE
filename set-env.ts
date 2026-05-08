67002c03f93c723c3ace744bead860d2dbccdcae
tuan-haui <2022605037.dhk17@st.haui.edu.vn>
2026-05-08 21:14:43 +0700

    update

--- set-env.ts
+++ set-env.ts
@@ -3,13 +3,25 @@
 const { writeFileSync } = require('node:fs');
 const { resolve } = require('node:path');
 
-const targetPath = resolve(process.cwd(), 'src/environments/environment.prod.ts');
-const apiTokenUrl = process.env['NG_APP_API_URL'] ?? 'https://api.feephim.com';
+const environmentPath = resolve(process.cwd(), 'src/environments/environment.ts');
+const environmentDevelopmentPath = resolve(process.cwd(), 'src/environments/environment.development.ts');
+const environmentProdPath = resolve(process.cwd(), 'src/environments/environment.prod.ts');
 
-const envConfigFile = `export const environment = {
+const apiTokenUrl = process.env['NG_APP_API_URL'] ?? 'https://api.feephim.com';
+const developmentApiTokenUrl = process.env['NG_APP_API_URL_DEV'] ?? 'https://localhost:8080';
+
+const productionConfigFile = `export const environment = {
   production: true,
   API_TOKEN_URL: '${apiTokenUrl}'
 };
 `;
 
-writeFileSync(targetPath, envConfigFile, { encoding: 'utf8' });
+const developmentConfigFile = `export const environment = {
+  production: false,
+  API_TOKEN_URL: '${developmentApiTokenUrl}'
+};
+`;
+
+writeFileSync(environmentPath, productionConfigFile, { encoding: 'utf8' });
+writeFileSync(environmentDevelopmentPath, developmentConfigFile, { encoding: 'utf8' });
+writeFileSync(environmentProdPath, productionConfigFile, { encoding: 'utf8' });
